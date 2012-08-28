293ee6ee4460b558b44bfe69079e4b784ea41adf
Thorben Schröder <stillepost@gmail.com>
2012-08-28 10:48:51 +0200

    Beautifies devcenter service tests and adds some more endpoints to it.

--- app/js/services.js
+++ app/js/services.js
@@ -4,20 +4,50 @@
 
 var devcenterBackendService = angular.module('devcenterTest.services', []);
 devcenterBackendService.factory('devcenterClient', ['$http', '$q', function(http, q) {
-  var devcenterClient = function(datastoreBackendUrl, graphBackendUrl) {
+  http.makeRequest = function(options) {
+    var method = options.method;
+    var url = options.url;
+    var deferred = q.defer();
+
+    http({method: method, url: url}).
+      success(function(data, status, headers, config) {
+        if (options.returns !== undefined) {
+          deferred.resolve(options.returns(data, status, headers, config));
+        } else {
+          deferred.resolve();
+        }
+      }).
+      error(function(data, status, headers, config) {
+        deferred.reject(data.error);
+     });
+
+    return deferred.promise;
+  };
+
+  var devcenterClient = function(devcenterBackendUrl) {
     return {
       promoteDeveloper: function(uuid) {
-        var deferred = q.defer();
+        return http.makeRequest({
+          method: 'POST',
+          url: devcenterBackendUrl + '/developers/' + uuid
+        });
+      },
 
-        http({method: 'GET', url: '/someUrl'}).
-          success(function(data, status, headers, config) {
-            deferred.resolve();
-          }).
-          error(function(data, status, headers, config) {
-            deferred.reject(data.error);
-          });
+      demoteDeveloper: function(uuid) {
+        return http.makeRequest({
+          method: 'DELETE',
+          url: devcenterBackendUrl + '/developers/' + uuid
+        });
+      },
 
-        return deferred.promise;
+      listGames: function(developerUuid) {
+        return http.makeRequest({
+          method: 'GET',
+          url: devcenterBackendUrl + '/developers/' + developerUuid + '/games',
+          returns: function(data) {
+            return data;
+          }
+        });
       }
     };
   };
--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -3,8 +3,7 @@
 describe("devcenterClient", function() {
   var $httpBackend, devcenterClient;
 
-  var datastoreBackendUrl = 'http://datastore.example.com';
-  var graphBackendUrl     = 'http://graph.example.com';
+  var devcenterBackendUrl = 'http://devcenter.example.com';
 
   var randomUuid = function() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
@@ -13,6 +12,56 @@ describe("devcenterClient", function() {
     });
   };
 
+  var carryOut = function(action) {
+    var success, result, failure, error;
+    success = false;
+    result  = null;
+    failure = false;
+    error   = null;
+
+    action.then(function(callResult) {
+       success = true;
+       result = callResult;
+    }, function(reason) {
+      failure = true;
+      error = reason;
+    });
+    $httpBackend.flush();
+
+    return {success: success, result: result, failure: failure, error: error};
+  };
+
+  var can = function(description, options) {
+    options = options || {};
+    var url = options.url;
+    var verb = options.verb;
+    var action = options.action;
+    var successfulResponse = options.response || '';
+    var expectations = options.expectations;
+
+    describe(description, function() {
+      verb = verb.toUpperCase();
+
+      beforeEach(function() {
+        $httpBackend['expect' + verb](url());
+      });
+
+      it('works', function() {
+        $httpBackend.when(verb, url()).respond(successfulResponse);
+        var response = carryOut(action());
+        expect(response).toBeSuccessful();
+        if (expectations !== undefined) {
+          expectations(response);
+        }
+      });
+
+      it('can fail', function() {
+        $httpBackend.when(verb, url()).respond(403, {error: 'some reason'});
+        expect(carryOut(action())).toErrorOutFor('some reason');
+      });
+    });
+  }
+
   var entity1, entity2;
 
   beforeEach(function() {
@@ -20,12 +69,20 @@ describe("devcenterClient", function() {
 
     inject(function($injector) {
       $httpBackend = $injector.get('$httpBackend');
-
-      devcenterClient = $injector.get('devcenterClient')(datastoreBackendUrl, graphBackendUrl);
+      devcenterClient = $injector.get('devcenterClient')(devcenterBackendUrl);
     });
 
-    var entity1 = randomUuid();
-    var entity2 = randomUuid();
+    this.addMatchers({
+      toBeSuccessful: function() {
+        return this.actual.success === true && this.actual.failure === false;
+      },
+      toErrorOutFor: function(reason) {
+        return this.actual.success === false && this.actual.failure === true && this.actual.error == reason;
+      }
+    });
+
+    entity1 = randomUuid();
+    entity2 = randomUuid();
   });
 
   afterEach(function() {
@@ -33,19 +90,25 @@ describe("devcenterClient", function() {
     $httpBackend.verifyNoOutstandingRequest();
   });
 
-  it('can promote developers', function() {
-    $httpBackend.when('POST', 'developers/' + entity1).respond('');
-    $httpBackend.expectPOST('/developers/' + entity1);
+  can('promote developers', {
+      verb: 'POST',
+      url: function() {return devcenterBackendUrl + '/developers/' + entity1},
+      action: function() {return devcenterClient.promoteDeveloper(entity1)}
+  });
 
-    var success = false;
-    var failure  = false;
-    devcenterClient.promoteDeveloper(entity1).then(function() {
-      success = true;
-    }, function() {
-      failure = true;
-    });
-    $httpBackend.flush();
-    expect(success).toBeTruthy();
-    expect(failure).toBeFalsy();
+  can('demote developers', {
+      verb: 'DELETE',
+      url: function() {return devcenterBackendUrl + '/developers/' + entity1},
+      action: function() {return devcenterClient.demoteDeveloper(entity1)}
+  });
+
+  can('list games', {
+      verb: 'GET',
+      url: function() {return devcenterBackendUrl + '/developers/' + entity1 + '/games'},
+      response: ['game1', 'game2'],
+      action: function() {return devcenterClient.listGames(entity1)},
+      expectations: function(response) {
+        expect(response.result).toEqual(['game1', 'game2']);
+      }
   });
 });
