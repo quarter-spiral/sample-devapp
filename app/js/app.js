dbb1faef8e4a53ca66179e3b10f61082a0c3c486
Alexander Kohlhofer <alexander@kohlhofer.com>
2012-09-26 19:05:38 -0700

    span details section out (final piece)

--- app/js/app.js
+++ app/js/app.js
@@ -13,5 +13,6 @@ angular.module('devcenterTest', ['ngCookies', 'devcenterTest.filters', 'devcente
     $routeProvider.when('/games/:gameUuid/venues', {templateUrl: 'partials/games/venues.html', controller: 'GamesCtrl'});
     $routeProvider.when('/games/:gameUuid/builds', {templateUrl: 'partials/games/builds.html', controller: 'GamesCtrl'});
     $routeProvider.when('/games/:gameUuid/developers', {templateUrl: 'partials/games/developers.html', controller: 'GamesCtrl'});
+    $routeProvider.when('/games/:gameUuid/details', {templateUrl: 'partials/games/details.html', controller: 'GamesCtrl'});
     $routeProvider.otherwise({redirectTo: '/'});
   }]);
