a5acb460ea7b009c0a524f17b2809802487233d3
Thorben Schröder <stillepost@gmail.com>
2012-12-06 16:57:19 +0100

    Fixes several small issues Alex had

--- app/js/filters.js
+++ app/js/filters.js
@@ -7,4 +7,9 @@ angular.module('devcenterTest.filters', []).
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     }
+  }]).
+  filter('canvasUrl', [function() {
+    return function(gameUuid, venue) {
+      return String(qs.ENV.QS_CANVAS_APP_URL + "/v1/games/" + gameUuid + "/" + venue);
+    };
   }]);
