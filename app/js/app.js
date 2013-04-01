d08c40c2b6cb4ca0baae5f7650b5368ec717144c
Alexander Kohlhofer <alexander@kohlhofer.com>
2013-04-01 14:49:51 -0400

    removed developer/team section link and route

--- app/js/app.js
+++ app/js/app.js
@@ -17,7 +17,7 @@ angular.module('devcenterTest', ['ngCookies', 'devcenterTest.filters', 'devcente
     $routeProvider.when('/games/:gameUuid/venues/facebook', {templateUrl: 'partials/games/venues/facebook.html', controller: 'GamesCtrl'});
     $routeProvider.when('/games/:gameUuid/venues/embedded', {templateUrl: 'partials/games/venues/embedded.html', controller: 'GamesCtrl'});
     $routeProvider.when('/games/:gameUuid/builds', {templateUrl: 'partials/games/builds.html', controller: 'GamesCtrl'});
-    $routeProvider.when('/games/:gameUuid/developers', {templateUrl: 'partials/games/developers.html', controller: 'GamesCtrl'});
+    //$routeProvider.when('/games/:gameUuid/developers', {templateUrl: 'partials/games/developers.html', controller: 'GamesCtrl'});
     $routeProvider.when('/games/:gameUuid/details', {templateUrl: 'partials/games/details.html', controller: 'GamesCtrl'});
     $routeProvider.when('/games/:gameUuid/dimensions', {templateUrl: 'partials/games/dimensions.html', controller: 'GamesCtrl'});
     $routeProvider.when('/games/:gameUuid/friendbar', {templateUrl: 'partials/games/friendbar', controller: 'GamesCtrl'});
