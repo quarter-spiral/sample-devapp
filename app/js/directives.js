d754bd1b7c82cb21fc681eeba84f98eda66d7970
Thorben Schröder <stillepost@gmail.com>
2013-05-23 18:55:25 +0200

    Puts fully working CORS workaroung for IE in place

--- app/js/directives.js
+++ app/js/directives.js
@@ -126,4 +126,4 @@ angular.module('devcenterTest.directives', []).directive('markdown', function() 
           });
         }
     };
-});;
+});
---
+++ app/lib/flXHR/angular-adaptor.js
@@ -0,0 +1,76 @@
+if (window.XDomainRequest !== undefined) {
+  var originalXMLHttpRequest = window.originalXMLHttpRequest
+  document.write('<script src="lib/flXHR/flXHR.js"></script>');
+
+  window.XMLHttpRequest = function() {
+    flXHR = new flensed.flXHR();
+
+    flXHR.autoUpdatePlayer = true;
+    flXHR.xmlResponseText = false;
+    flXHR.binaryResponseBody = true;
+
+    var originalGetAllResponseHeaders = flXHR.getAllResponseHeaders;
+    flXHR.getAllResponseHeaders = function() {
+      return originalGetAllResponseHeaders().join("\n")
+    };
+
+    var originalOpen = flXHR.open;
+    flXHR.open = function() {
+      this.__angular_flxhr_request_headers = [];
+      var method = arguments[0];
+      if (method !== 'POST' && method !== 'GET') {
+        flXHR.setRequestHeader('FAKE-METHOD', method)
+        method = 'POST';
+        arguments[0] = method;
+      }
+      this.__angular_flxhr_adaptor_http_method = method;
+      this.__angular_flxhr_original_arguments = arguments;
+    }
+
+    var originalSetRequestHeader = flXHR.setRequestHeader;
+    flXHR.setRequestHeader = function(header, value) {
+      if (header === 'Authorization' && value !== undefined && value.match !== undefined && value.match(/^Bearer /)) {
+        // Turn Authorization header into query param
+        var newArguments = this.__angular_flxhr_original_arguments;
+        var url = newArguments[1];
+        if (url.match(/\?/)) {
+          url = url + "&";
+        } else {
+          url = url + "?";
+        }
+        url = url + "oauth_token=" + value.replace(/^Bearer /, '');
+        this.__angular_flxhr_original_arguments[1] = url;
+      } else {
+        this.__angular_flxhr_request_headers.push([header, value])
+      }
+    }
+
+    var originalSend = flXHR.send;
+    flXHR.send = function() {
+      originalOpen.apply(this, this.__angular_flxhr_original_arguments);
+      var headersToSet = this.__angular_flxhr_request_headers;
+      for (var i = 0; i < headersToSet.length; i++) {
+        originalSetRequestHeader.call(this, headersToSet[i][0], headersToSet[i][1]);
+      }
+
+      if (this.__angular_flxhr_adaptor_http_method === 'POST' && (arguments[0] === undefined || arguments[0] === null || arguments[0] === "")) {
+        flXHR.setRequestHeader("Content-Type", "application/json");
+        arguments[0] = '{}';
+      }
+
+      flXHR.setRequestHeader("Content-Type", "application/json");
+
+      originalSend.apply(this, arguments);
+    }
+
+    // flXHR.onerror = function(errObj) {
+    //   console.log("Error: "+errObj.number
+    //       +"\nType: "+errObj.name
+    //       +"\nDescription: "+errObj.description
+    //       +"\nSource Object Id: "+errObj.srcElement.instanceId
+    //     );
+    // };
+
+    return flXHR;
+  }
+}
