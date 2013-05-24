c5a5b5d2ad900c974481e115e922fe2b4fd387f6
Thorben Schröder <stillepost@gmail.com>
2013-05-24 18:01:31 +0200

    Fixes AJAX caching issue in IE

--- app/lib/flXHR/angular-adaptor.js
+++ app/lib/flXHR/angular-adaptor.js
@@ -47,6 +47,17 @@ if (window.XDomainRequest !== undefined) {
 
     var originalSend = flXHR.send;
     flXHR.send = function() {
+      var newArguments = this.__angular_flxhr_original_arguments;
+      var url = newArguments[1];
+      if (url.match(/\?/)) {
+        url = url + "&";
+      } else {
+        url = url + "?";
+      }
+      var randomSeed =  Math.floor((1 + Math.random()) * 0x1000000).toString(16) + (new Date().getTime().toString());
+      url = url + "_id_random_seed=" + randomSeed;
+      this.__angular_flxhr_original_arguments[1] = url;
+
       originalOpen.apply(this, this.__angular_flxhr_original_arguments);
       var headersToSet = this.__angular_flxhr_request_headers;
       for (var i = 0; i < headersToSet.length; i++) {
